f6cef14651c7f27278f67a084a1d4032143c7c91
wagolemusa <homiemusa@gmail.com>
2023-07-08 21:52:40 +0300

    Add url

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:5000/api/v2";
+export const server = "https://busizchainbackend.onrender.com/api/v2";
 
-export const backend_url = "http://localhost:5000/"
+export const backend_url = "https://busizchainbackend.onrender.com/"
